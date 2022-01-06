9bcf5c734668566b213467ed11b9c9b88ada5441
hefadog2 <>
2022-01-06 18:42:47 -0500

    added pitch based catagorization to add variance:

--- dataacquisition/acquisition.py
+++ dataacquisition/acquisition.py
@@ -17,19 +17,42 @@ def analysis_func(track: str,spot_auth) -> any:
     analysis = spot_auth.audio_analysis(track)
     # features = spot_auth.audio_features(track)
     # features_df = pd.DataFrame(data=features, columns=features[0].keys())
-    beats_df = pd.DataFrame(data=analysis["beats"])
-    # segments_df = pd.DataFrame(data=analysis["segments"])
+    #beats_df = pd.DataFrame(data=analysis["beats"])
+    segments_df = pd.DataFrame(data=analysis["segments"])
     # bars_df = pd.DataFrame(data=analysis["bars"])
     # tatums_df = pd.DataFrame(data=analysis["tatums"])
-    return beats_df
+    return segments_df
 
-def beatmaker(beats):
+def beatmaker(segments):
     """appends the osu file with data"""
-    beat_start = beats["start"]
-    with open("osutesting.osu", 'a', encoding="utf-8") as beatmap:
-        for index, _ in beats.iterrows():
-            print("256,192,",beat_start[index]*1000,",5,4,0:0:0:0:\n", sep='')
-            beatmap.write("256,192,"+ str(beat_start[index]*1000) +",5,4,0:0:0:0:\n")
+    seg_start = segments["start"]
+    seg_pitch = segments["pitches"]
+    with open("ManiaTest.osu", 'a', encoding="utf-8") as beatmap:
+        for index, _ in segments.iterrows():
+            pitchavg = pitchaverage(seg_pitch[index])
+            print(pitchcat(pitchavg),",192,",seg_start[index]*1000,",5,4,0:0:0:0:\n", sep='')
+            beatmap.write("256,192,"+ str(seg_start[index]*1000) +",5,4,0:0:0:0:\n")
+
+def pitchaverage(seg_pitch):
+    """averages all piches in a section"""
+    temp = 0
+    for index in range( len(seg_pitch)):
+        temp += seg_pitch[index]
+    temp /= len(seg_pitch)-1
+    print(temp)
+    return temp
+
+def pitchcat(pitchavg):
+    """catagorizes pitches into four groups"""
+    if(pitchavg < 0.3):
+        temp = 128
+    elif(pitchavg < 0.4):
+        temp = 256
+    elif(pitchavg < 0.5):
+        temp = 384
+    else:
+        temp = 512
+    return temp
 
 def main():
     """declared variables and calls other funvtions"""
@@ -40,8 +63,8 @@ def main():
     spot_auth = authentication(cid,secret)
 
     track = "spotify:track:6yIjtVtnOBeC8SwdVHzAuF"
-    beats = analysis_func(track,spot_auth)
-    beatmaker(beats)
+    segments = analysis_func(track,spot_auth)
+    beatmaker(segments)
 
 if __name__ == "__main__":
     main()
